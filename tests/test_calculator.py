b04556604354e420b0f645654c914aff083f243d
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2025-03-04 09:50:32 +0100

    [feature/minmax] Add min & max feature

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,12 +2,18 @@ from unittest import TestCase
 from src.calculator import Calculator
 
 class TestCalculator(TestCase):
-    def setUp(self):
-        self.calc = Calculator()
+	def setUp(self):
+		self.calc = Calculator()
 
-    def test_sum(self):
-        self.assertEqual(self.calc.mysum(1, 2), 3)
+	def test_sum(self):
+		self.assertEqual(self.calc.mysum(1, 2), 3)
 
+
+	def test_min(self):
+		self.assertEqual(self.calc.mymin(3, 5), 3)
+
+	def test_max(self):
+		self.assertEqual(self.calc.mymax(3, 5), 5)
     
     
 if __name__ == '__main__':
