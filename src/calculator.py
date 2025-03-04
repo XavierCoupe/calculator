152c2761b5991b6f6c91b5e1494dd8bde6a4022c
XavierCoupe <123065496+XavierCoupe@users.noreply.github.com>
2025-03-04 09:51:18 +0100

    [Dev] Add mean functionality

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,8 @@
 class Calculator :
-    # return the sum between 2 operands
-    def mysum(self, first_operand, second_operand):
-        return first_operand + second_operand
+	# return the sum between 2 operands
+	def mysum(self, first_operand, second_operand):
+		return first_operand + second_operand
+    
+	# return the mean between 2 operands
+	def mymean(self, first_operand, second_operand):
+		return (first_operand+second_operand) / 2
