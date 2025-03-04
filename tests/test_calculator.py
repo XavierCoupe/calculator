6cb4bc8bc72a3cd55b19230d046c0b867bc6b51f
XavierCoupe <123065496+XavierCoupe@users.noreply.github.com>
2025-03-04 09:51:33 +0100

    [Test] Add mean functionality test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,13 +2,16 @@ from unittest import TestCase
 from src.calculator import Calculator
 
 class TestCalculator(TestCase):
-    def setUp(self):
-        self.calc = Calculator()
+	def setUp(self):
+  		self.calc = Calculator()
 
-    def test_sum(self):
-        self.assertEqual(self.calc.mysum(1, 2), 3)
+	def test_sum(self):
+  		self.assertEqual(self.calc.mysum(1, 2), 3)
+      
+	def test_mean(self):
+			self.assertEqual(self.calc.mymean(2,6),4)
 
     
     
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
